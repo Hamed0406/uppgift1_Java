1262e51f8ade502907778e5a4bdb230130a899d0
hamed0406 <h.adeli.meadeli@gmail.com>
2020-09-18 18:03:27 +0200

    w38

---
+++ src/W28/Extra3.java
@@ -0,0 +1,29 @@
+package W28;
+/*
+* Skriv ett program som tar in en Int och som sedan skriver ut summan av siffrorna:
+
+Input Data:
+Input an integer: 25
+Expected Output
+
+The sum of the digits is: 7
+* */
+
+import java.util.Scanner;
+
+public class Extra3 {
+    public static  void main(String args[]){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Please enter your number : ");
+        int inputInt= scanner.nextInt();
+        int sum=0;
+        int tempSum;
+        while(inputInt>0){
+         tempSum=inputInt%10;
+         inputInt=inputInt/10;
+         sum=sum+tempSum;
+        }
+ System.out.print("Sum of digits are : "+sum);
+
+    }
+}
