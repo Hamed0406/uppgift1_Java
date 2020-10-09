ff10943c741612b5022031e1ee9b0514591bc7ff
hamed0406 <h.adeli.meadeli@gmail.com>
2020-10-09 18:24:44 +0200

    W41

--- src/Inlamningsuppgift_two/Uppgifttwo.java
+++ src/Inlamningsuppgift_two/Uppgifttwo.java
@@ -1,4 +1,8 @@
 package Inlamningsuppgift_two;
+
+import java.util.Random;
+import java.util.Scanner;
+
 /*
 * Ett lottoprogram slumpar fram ett tvåsiffrigt tal mellan 01 och 99. Om användare gissar rätt nummer i rätt ordning, vinner användaren 10 000 kr. Om användaren gissar rätt nummer, men i fel ordning, vinner användaren 5 000 kr. Om användare gissar ett rätt nummer av de två, vinner användaren 1 000 kr.
 
@@ -7,6 +11,57 @@ Som lottospelare vill jag kunna mata in ett tvåsiffrigt nummer i ett program oc
 public class Uppgifttwo {
 
     public static void main(String args[]){
+        final String TEXT_YELLOW = "\u001B[33m";
+        final String TEXT_RESET = "\u001B[0m";
+        final String TEXT_BLUE = "\u001B[34m";
+        final String TEXT_BOLD ="\u001B[1m";
+        final String TEXT_RED="\u001B[31m";
+        int max=99;
+        int min= 1;
+        int winNumber,userInput;
+        int prize = 0,bank;
+        boolean flag=true;
+        String winNumberString,userInputString;
+        Random r = new Random();
+        winNumber= r.nextInt((max - min) + 1) + min;
+
+        System.out.println(winNumber);
+
+        System.out.print("Hi welcome to lottery program.This is first time you are using the system you get one free ticket \nyou pick two a digits number.\nIf you guess right digits in right order ;you win 1000SEK .\nIf you guess right digits ,but not in right order ;you win 5000 SEK.\n if you guess one right digits ; you win 1000 SEK . ");
+        System.out.print("If you you are ready to play enter your two digits without any space. ");
+       do {
+           Scanner sc = new Scanner(System.in);
+           userInput = sc.nextInt();
+           //control user input  and random number ,converted them in correct order.
+           if (winNumber > 0 && winNumber <= 9)
+               winNumberString = String.format("%02d", winNumber);
+           else
+               winNumberString = Integer.toString(winNumber);
+           if (userInput > 0 && userInput <= 9)
+               userInputString = String.format("%02d", userInput);
+           else
+               userInputString = Integer.toString(userInput);
+           if (winNumberString.equals(userInputString))
+               prize = 10000;
+           else if ((winNumberString.charAt(0) == userInputString.charAt(1)) && (winNumberString.charAt(1) == userInputString.charAt(0)))
+               prize = 5000;
+           else if ((userInputString.charAt(0)==winNumberString.charAt(0))||(userInputString.charAt(0)==winNumberString.charAt(1))||(userInputString.charAt(1)==winNumberString.charAt(0))||(userInputString.charAt(1)==winNumberString.charAt(1)))
+               prize = 1000;
+           else if((userInputString.charAt(0)!=winNumberString.charAt(0))&&(userInputString.charAt(0)!=winNumberString.charAt(1))&&(userInputString.charAt(1)!=winNumberString.charAt(0))&&(userInputString.charAt(1)!=winNumberString.charAt(1)))
+               System.out.println("you lost");
+           System.out.println(prize);
+           prize=0;
+          // flag=false;
+
+       }
+       while(flag);
+
+
+
+
+
+          //  System.out.println(prize);
+
 
     }
 }
