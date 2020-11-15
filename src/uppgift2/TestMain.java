ee2ee08b6a7183d53200c9d209b08b435b7e6f6b
hamed0406 <h.adeli.meadeli@gmail.com>
2020-11-15 12:02:12 +0100

    Test Main had been created and tested .

---
+++ src/uppgift2/TestMain.java
@@ -0,0 +1,10 @@
+package uppgift2;
+
+public class TestMain {
+    public static void main(String args[])
+    {
+
+        System.out.println("First Test");
+
+    }//end of main
+}//End of class TestMain
