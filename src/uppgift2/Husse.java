7124a599ff7225fbee5aec9c0ddb2f336d96f7da
hamed0406 <h.adeli.meadeli@gmail.com>
2020-11-18 08:08:36 +0100

    Create Uml Husse and Relation to Dog.

---
+++ src/uppgift2/Husse.java
@@ -0,0 +1,51 @@
+package uppgift2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* Definiera en klass Husse. Definiera lämpliga operationer så att Husse har ett namn, kan äga många hundar samt kan tala om vad han heter och vilka hundar han äger.*/
+public class Husse {
+    String husseName;
+    ArrayList<Dog> dogArrayListist=new ArrayList<Dog>();//List of Dog for a owner .
+
+    public Husse(String husseName, ArrayList<Dog> dogArrayList) {
+        this.husseName = husseName;
+        this.dogArrayListist = dogArrayList;
+    }
+
+    public String getHusseName() {
+        return husseName;
+    }
+
+    public void setHusseName(String husseName) {
+        this.husseName = husseName;
+    }
+
+    public ArrayList<Dog> getDogArrayList() {
+        return dogArrayListist;
+    }
+
+
+    public void addToDogList(Dog theDog)
+    {
+        dogArrayListist.add(theDog);
+    }
+    public void printDogOwnerNameAndDogs()
+    {
+        System.out.println("Dogs owner name is : "+husseName+" He is owner of ");
+      int index=  dogArrayListist.size();//Number of element in the list .
+        for(int i=0;i<index;i++)
+        {
+            System.out.println("Dog name is : "+ dogArrayListist.get(i).getDogName()+" Dog race is : "+dogArrayListist.get(i).getDogRace());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Husse{" +
+                "husseName='" + husseName + '\'' +
+                ", dogArrayListist=" + dogArrayListist +
+                '}';
+    }
+}//End of Husse  class
--- src/uppgift2/TestMain.java
+++ src/uppgift2/TestMain.java
@@ -1,5 +1,7 @@
 package uppgift2;
 
+import java.util.ArrayList;
+
 public class TestMain {
     public static void main(String [] args)
     {
@@ -13,7 +15,21 @@ System.out.println("Test Dog name is: "+testDog.getDogName()+"\nTest Dog race is
 
         /*EN to EN demonstration */
         Matte testMatte=new Matte("Petra",testCatt);//Send object cat to the constructor .
-        System.out.println("Cat owner name is : "+testMatte.getMamaOwnerName()+" Name of cat is : "+testMatte.getCatAsPet().getCattName()+" Cat race is : "+testMatte.getCatAsPet().getCatRace());
+        System.out.println("Cat owner name is : "+testMatte.getMamaOwnerName()+"\n Name of cat is : "+testMatte.getCatAsPet().getCattName()+"\n Cat race is : "+testMatte.getCatAsPet().getCatRace());
+/*( En -> Många ) demonstration */
+        ArrayList<Dog> testDogArrayList=new ArrayList<Dog>();
+        testDogArrayList.add(testDog);
+
+        Husse testHusse=new Husse("Hamed",testDogArrayList);
+        Dog testDog1,testDog2;
+        testDog1 = new Dog("Wolfy","GermanShaper");//set name and race by conductor for test1 .
+        testDog2=new Dog("Sgorty","Husky");//set name and race by conductor for test2 .
+
+        testDogArrayList.add(testDog1);//Add to the list
+        testDogArrayList.add(testDog2);//Add to the list
+
+        testHusse.printDogOwnerNameAndDogs();
+
 
 
 
