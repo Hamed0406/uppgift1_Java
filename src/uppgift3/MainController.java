530dbda2a94a569f28087f99dab872494bce7a82
Hamed0406 <h.adeli.meadeli@gmail.com>
2020-12-02 13:18:35 +0100

    Reformat code and clean oup.

--- src/uppgift3/MainController.java
+++ src/uppgift3/MainController.java
@@ -2,21 +2,29 @@ package uppgift3;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Duration;
 
 import javax.imageio.IIOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * @author Hamed Adeli,
  * h.adeli.meadeli@gmail.com
  * 11/29/2020
  */
-public class MainController {
+public class MainController implements Initializable {
 
     @FXML
     private Label lblTime;
@@ -26,9 +34,17 @@ public class MainController {
     private Button btnExit;
     @FXML
     private Button btnReset;
+    @FXML
+    private TableView<TimeHistory> tblTimerHistory;
+    @FXML
+    private TableColumn<TimeHistory, String> timeStampData;
+    @FXML
+    private TableColumn<TimeHistory, String> lastTimeData;
     private boolean timeFlagStart = true;
     private Timeline timeline;
     private int min = 0, sec = 0, milliS = 0;
+    private String lastTime;
+    private String timeStamp;
 
 
     public void exit(ActionEvent event) throws IIOException {
@@ -49,6 +65,7 @@ public class MainController {
         lblTime.setText((((min / 10) == 0) ? "0" : "") + min + ":"
                 + (((sec / 10) == 0) ? "0" : "") + sec + ":"
                 + (((milliS / 10) == 0) ? "00" : (((milliS / 100) == 0) ? "0" : "")) + milliS++ + " Secs");
+        lastTime = lblTime.getText();
     }
 
     public void start() {
@@ -76,6 +93,15 @@ public class MainController {
                     timeline.pause();
                     timeFlagStart = true;
                     btnStart.setText("Start");
+                    timeStamp = java.time.LocalDateTime.now().toString();
+                    //TODO Popout time stamp and Last Time in Tableview.
+                    TimeHistory timeHistory = new TimeHistory(timeStamp, lastTime);
+                    System.out.println(timeHistory);
+                     ObservableList<TimeHistory> list = FXCollections.observableArrayList(
+                            new TimeHistory(timeStamp, lastTime)
+                    );
+
+
                 }
             }
         });
@@ -97,6 +123,15 @@ public class MainController {
     }
 
 
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle,ObservableList list) {
+        timeStampData.setCellValueFactory(new PropertyValueFactory<TimeHistory, String>("timeStampData"));
+        lastTimeData.setCellValueFactory(new PropertyValueFactory<TimeHistory, String>("lastTimeData"));
+        tblTimerHistory.setItems(list);
+
+    }
 }
 
 
