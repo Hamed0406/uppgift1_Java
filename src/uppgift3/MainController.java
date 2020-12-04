de2531342456222383269a995f8ff5e8754ff2dc
Hamed0406 <h.adeli.meadeli@gmail.com>
2020-12-04 21:31:53 +0100

    Add CSS style and cleanup code .

--- src/uppgift3/MainController.java
+++ src/uppgift3/MainController.java
@@ -6,7 +6,6 @@ import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -16,7 +15,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Duration;
 
-import javax.imageio.IIOException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -30,39 +28,39 @@ import java.util.ResourceBundle;
  */
 public class MainController implements Initializable {
 
-    @FXML
-    private Label lblTime;
+    public ObservableList<TimeHistory> list = FXCollections.observableArrayList();//Creat observable Array List for using in table view .
     @FXML
     private Button btnStart;
     @FXML
-    private Button btnExit;
+    private Label lblTime;//Label Shows stopwatch timer.
     @FXML
-    private Button btnReset;
+    private Button btnExit;//Button for exiting app.
     @FXML
-    private TableView<TimeHistory> tblTimerHistory;
+    private Button btnReset;//Button for Reset the stopwatch and clean time Table history app.
     @FXML
-    private TableColumn<TimeHistory, String> timeStampData;
+    private TableView<TimeHistory> tblTimerHistory;//Table view of history timer.
     @FXML
-    private TableColumn<TimeHistory, String> lastTimeData;
-    public ObservableList<TimeHistory> list = FXCollections.observableArrayList(
-            new TimeHistory(null, null)
-    );
+    private TableColumn<TimeHistory, String> timeStampData;//Time stamp att stop Stopwatch.
+    @FXML
+    private TableColumn<TimeHistory, String> lastTimeData;//Last amount of timer stopwatch.
     @FXML
     private boolean timeFlagStart = true;
-    private Timeline timeline;
     @FXML
     private Label lblClock;
+    private Timeline timeline;
+    private Timeline clock;
+
     private int min = 0, sec = 0, milliS = 0;
     private String lastTime;
     private String timeStamp;
 
-
-    public void exit(ActionEvent event) throws IIOException {
+    //Exit function .
+    public void exit(ActionEvent event) {
         System.exit(0);
 
     }
-    private Timeline clock;
 
+    //Stopwatch timer function.
     public void changeTime(Label lblTime) {
         if (milliS == 1000) {
             sec++;
@@ -78,48 +76,44 @@ public class MainController implements Initializable {
         lastTime = lblTime.getText();
     }
 
+    //Start Stopwatch function.
     public void start() {
 
-        digitalClock();
-        btnStart.setText("Start");
-        timeline = new Timeline(new KeyFrame(Duration.millis(1), new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-
-                changeTime(lblTime);
-            }
-        }));
+        digitalClock();//Show digital clock after setting the stopwatch.
+        lblTime.setText("00:00:000");
+        btnStart.setText("Start");//Set Start test to the button.
+        //Initialize  timer timeline
+        timeline = new Timeline(new KeyFrame(Duration.millis(1), event -> changeTime(lblTime)));
 
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.setAutoReverse(false);
 
-        btnStart.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (timeFlagStart) {
+        btnStart.setOnAction(event -> {
+            //Start and stop and set logic alternative .
+            if (timeFlagStart) {
 
-                    timeline.play();
-                    timeFlagStart = false;
-                    btnStart.setText("Stop");
-                } else {
-                    timeline.pause();
-                    timeFlagStart = true;
-                    btnStart.setText("Start");
-                    LocalDateTime now = LocalDateTime.now();
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                    String timeStamp = now.format(formatter);
-                    TimeHistory timeHistory = new TimeHistory(timeStamp, lastTime);
-                    list.add(timeHistory);
+                timeline.play();
+                timeFlagStart = false;
+                btnStart.setText("Stop");
+            } else {
+                timeline.pause();
+                timeFlagStart = true;
+                btnStart.setText("Start");
+                //format time for more convince output.
+                LocalDateTime now = LocalDateTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                String timeStamp = now.format(formatter);
+                TimeHistory timeHistory = new TimeHistory(timeStamp, lastTime);
+                list.add(timeHistory);//Add Data to the Tableview.
 
 
-                }
             }
         });
 
 
     }
 
+    //Reset and clear history function.
     public void reset(ActionEvent event) {
         min = 0;
         sec = 0;
@@ -135,12 +129,13 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        timeStampData.setCellValueFactory(new PropertyValueFactory<TimeHistory, String>("timeStampData"));
-        lastTimeData.setCellValueFactory(new PropertyValueFactory<TimeHistory, String>("lastTimeData"));
+        timeStampData.setCellValueFactory(new PropertyValueFactory<>("timeStampData"));
+        lastTimeData.setCellValueFactory(new PropertyValueFactory<>("lastTimeData"));
         tblTimerHistory.setItems(list);
 
     }
 
+    //Clock function
     public void digitalClock() {
 
         Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
--- src/uppgift3/TimeHistory.java
+++ src/uppgift3/TimeHistory.java
@@ -6,20 +6,21 @@ import javafx.beans.property.SimpleStringProperty;
  * @author Hamed Adeli,
  * h.adeli.meadeli@gmail.com
  * 12/1/2020
+ * This calss contain a model for Time Table Hisotry .
  */
 public class TimeHistory {
-    private final SimpleStringProperty timeStampData;
-    private final SimpleStringProperty lastTimeData;
+    private final SimpleStringProperty timeStampData;//Time stamp when stop stopWatch function.
+    private final SimpleStringProperty lastTimeData;//Time at timer when stop stopWatch function.
+
     public TimeHistory(String timeStampData, String lastTimeData) {
         super();
-        this.timeStampData =  new SimpleStringProperty(timeStampData);
+        this.timeStampData = new SimpleStringProperty(timeStampData);
         this.lastTimeData = new SimpleStringProperty(lastTimeData);
     }
 
     public String getTimeStampData() {
         return timeStampData.get();
     }
-
 
 
     public String getLastTimeData() {
