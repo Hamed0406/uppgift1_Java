60f04c796d140bd1ee0529adae1fd66a28757bb8
Hamed0406 <h.adeli.meadeli@gmail.com>
2020-12-06 09:07:18 +0100

    Change design new skeleton.

--- src/uppgift4/MainApp.java
+++ src/uppgift4/MainApp.java
@@ -1,32 +1,28 @@
 package uppgift4;
 
-/**
- * @author Hamed Adeli,
- * h.adeli.meadeli@gmail.com
- * 12/5/2020
- */
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import uppgift4.models.Person;
 
-public class MainPersson extends Application {
+/**
+ * @author Hamed Adeli,
+ * h.adeli.meadeli@gmail.com
+ * 12/6/2020
+ */
+public class MainApp extends Application {
 
-    private ObservableList<Person> personData = FXCollections.observableArrayList();
 
     @Override
     public void start(Stage primaryStage) {
         try {
 
-            Parent root = FXMLLoader.load(getClass().getResource("views/MainPersson.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("views/MainApp.fxml"));
 
             Scene scene = new Scene(root);
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-            primaryStage.setTitle("Stop Watch");
+            primaryStage.setTitle("Person DataBase");
 
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -40,9 +36,4 @@ public class MainPersson extends Application {
         launch(args);
     }
 
-
-    public ObservableList<Person> getPerssonData()
-    {
-        return personData;
-    }
 }
--- src/uppgift4/controllers/MainController.java
+++
@@ -1,9 +0,0 @@
-package uppgift4.controllers;
-
-/**
- * @author Hamed Adeli,
- * h.adeli.meadeli@gmail.com
- * 12/5/2020
- */
-public class MainController {
-}
