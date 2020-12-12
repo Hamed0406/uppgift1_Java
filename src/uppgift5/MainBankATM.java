7bb2d9c71b8b742d551f1ba8cd3f59300e210847
Hamed0406 <h.adeli.meadeli@gmail.com>
2020-12-12 13:39:44 +0100

    Create login page and skeleton of ATM App.

---
+++ src/uppgift5/MainBankATM.java
@@ -0,0 +1,39 @@
+package uppgift5;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Hamed Adeli,
+ * h.adeli.meadeli@gmail.com
+ * 12/11/2020
+ */
+
+public class MainBankATM extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("views/Login.fxml"));
+
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setTitle("Welcome to ATM With JavaFX");
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
