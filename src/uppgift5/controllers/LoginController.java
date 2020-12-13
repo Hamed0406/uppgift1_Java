ecf583ac43e5d796f2e4d344d9468d63e3429634
Hamed0406 <h.adeli.meadeli@gmail.com>
2020-12-13 20:46:27 +0100

    Creat SignUpFormView.XML and add functionality to change view .

--- src/uppgift5/controllers/LoginController.java
+++ src/uppgift5/controllers/LoginController.java
@@ -2,11 +2,18 @@ package uppgift5.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 /**
  * @author Hamed Adeli,
@@ -32,7 +39,7 @@ public class LoginController {
         stage.close();
     }
 
-    public void setBtnLogin(ActionEvent event) {
+    public void login(ActionEvent event) {
         if (txUserID.getText().isBlank()==false&&txPass.getText().isBlank() ==false) {
 validateLogin();
 
@@ -44,5 +51,19 @@ validateLogin();
 
     public void validateLogin() {
 
+//TODO get result from XML file
+        //Validate from input and go to next page
+
+
+    }
+
+    public void fromLoginToSignUpForm(ActionEvent event) throws IOException {
+//TODO change Scene to SignUP from.
+        Parent formViewParent= FXMLLoader.load(getClass().getResource("../views/SignUpFormView.fxml"));
+        Scene  formViewScene= new Scene(formViewParent);
+//Get stage Information
+        Stage window=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(formViewScene);
+        window.show();
     }
 }
